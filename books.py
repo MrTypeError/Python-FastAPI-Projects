f30158b5d3762cb173efd0f23abf2ab9768c3a74
Sudip Dutta <sudipexplorer@gmail.com>
2026-01-16 23:44:57 +0530

    Implimenting GET req. and Path parameters

---
+++ books.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
+
+
+@app.get("/books")
+async def read_all_books():
+    # return {'message' : 'Hello Sudip'}
+    return BOOKS
+
+# Path Parameters : These are the dynamic Parameters that are passed in the API calls to go to a dynamic location
+
+# VVV IMP #
+# We should always put the static call for any API call because if we don't keep it before the dynamic call then FastAPI will always call the dynamic call first.so the static call will never be executed.
+
+# @app.get("/books/{dynamic_param}")
+# async def list_all_books(dynamic_param : str):
+#     return {'dynamic Param' : dynamic_param}
+
+# @app.get("/books/{title_}") # Here in the call " " -> %20 (this is a place holder for space in FastAPI)
+# async def list_all_books():
+#     return {'dynamic Param' : dynamic_param}
+
+
+@app.get("/books/{book_title}")
+async def list_all_books(book_title : str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold() :
+            return book
