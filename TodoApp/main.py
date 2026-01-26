c0c518a37be541297922788730c57e86b174dc68
MrTypeError <sanugta8@gmail.com>
2026-01-26 15:32:24 +0530

    Implimented the Post method to add Todos

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -5,6 +5,7 @@ import models
 from database import engine, SessionLocal
 from models import Todos
 from starlette import status
+from pydantic import BaseModel, Field
 
 
 app = FastAPI()
@@ -20,6 +21,16 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+
+class TodoRequest(BaseModel):
+    title : str = Field(min_length=3)
+    description : str = Field(min_length=3, max_length=100)
+    priority : int = Field(gt=0, lt=6)
+    complete : bool
+
+
+
+
 @app.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
@@ -31,3 +42,12 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)) :
     if todo_model is not None :
         return todo_model
     return HTTPException(status_code = 404, detail='Todo not found.')
+
+
+@app.post("/todo",status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest) : 
+    todo_model = Todos(**todo_request.model_dump())
+
+    db.add(todo_model)
+    db.commit()
+
