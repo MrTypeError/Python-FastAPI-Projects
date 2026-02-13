93e67d3ee804f9c6704f456836e6ae26eb2cb48c
MrTypeError <sanugta8@gmail.com>
2026-02-14 00:36:04 +0530

    Implimenting a function to check wheather user Exists in DB or not

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -6,6 +6,7 @@ from models import Users
 from starlette import status
 from database import SessionLocal
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
@@ -30,6 +31,15 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password , user.hashed_password):
+        return False
+    return True
+
+
 
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency,
@@ -47,7 +57,14 @@ async def create_user(db: db_dependency,
     db.commit()
 
 
-
     return {'user' : 'authenticated'}
 
 
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm , Depends()],
+                                 db: db_dependency):
+    user = authenticate_user(form_data.username , form_data.password , db)
+    if not user :
+        return " Auth Faild !!! "
+    return "Successfull Auth Done !!!"
