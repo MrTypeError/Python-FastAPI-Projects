0e86a041d675ef86e5500efce0377081d3256ccb
MrTypeError <sanugta8@gmail.com>
2026-02-16 16:27:36 +0530

    Done some code cleaning in the auth file and implimented authorization in the POST API

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -10,14 +10,17 @@ from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2AuthorizationCodeBearer
 from jose import jwt,JWTError
 
-router = APIRouter()
+router = APIRouter(
+    prefix='/auth',
+    tags=['auth']
+)
 
 
 SECRET_KEY = '1a3ced0b072420132969ba446b848c13ed8271f05345df9e35c345f1b650e316'
 ALGORITHM = 'HS256'
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated = 'auto')
-oauth2_bearer = OAuth2AuthorizationCodeBearer(tokenUrl='token')
+oauth2_bearer = OAuth2AuthorizationCodeBearer(tokenUrl='auth/token')
 
 
 class CreateUserRequest(BaseModel):
@@ -69,7 +72,7 @@ async def  get_current_user(token: Annotated[str, Depends(oauth2_bearer)]) :
 
 
 
-@router.post("/auth", status_code=status.HTTP_201_CREATED)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency,
                       create_user_request: CreateUserRequest):
     create_user_model = Users(
@@ -94,6 +97,10 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm 
                                  db: db_dependency):
     user = authenticate_user(form_data.username , form_data.password , db)
     if not user :
-        return " Auth Faild !!! "
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail='Could not validate user.')
+    
     token = create_access_token(user.username, user.id, timedelta(minutes=20))
-    return token
+
+    return {'access_token' : token, 'token_type' : 'bearer'}
+
