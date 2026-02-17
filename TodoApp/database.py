b3c65d715278e8ecc9bbdeed13d3c7660aca25e5
MrTypeError <sanugta8@gmail.com>
2026-02-17 22:59:27 +0530

    Used alembic to alter columns in existing table and also learned to downgrade the changes

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Sanu@2025!@localhost/TodoApplication'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Sanu%402025%21@localhost/TodoApplication'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
