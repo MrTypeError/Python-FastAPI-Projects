0cbfeade83a21a4a78db582cecd5340e12a2d01b
MrTypeError <sanugta8@gmail.com>
2026-02-18 22:50:08 +0530

    added heat_hcheck functionality & implimented change password and added phone numbers using alembic

--- TodoApp/routers/users.py
+++ TodoApp/routers/users.py
@@ -53,3 +53,12 @@ async def update_password(user: user_dependency, db: db_dependency, user_verific
     db.commit()
 
 
+@router.put("/phonenumber/{updated_phone_number}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_phone_number(user: user_dependency, db: db_dependency, phone_number: str):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    
+    user_details = db.query(Users).filter(Users.id == user.get('id')).first()
+    user_details.phone_number = phone_number
+    db.add(user_details)
+    db.commit()
